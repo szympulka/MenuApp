43a4cd1d4747a5bb1fef3df61165c76b5b317597
szympulka <szympulka@gmail.com>
2017-03-18 09:36:22 +0100

    HomeView

--- MenuApp.Web/scripts/Home/Home.js
+++ MenuApp.Web/scripts/Home/Home.js
@@ -9,8 +9,20 @@
 });
 function burger(x) {
 
+    var dayName = $('.DayName')
     x.classList.toggle("change");
     var burgermenu = $('.burger-menu');
     burgermenu.toggle("slow");
 
+    if ($('.DayName').hasClass('hide_panel'))
+    {
+        $('.DayName').removeClass('hide_panel');
+        $('.DayName').addClass('show_panel');
+    }
+    else
+    {
+        $('.DayName').addClass('hide_panel');
+        $('.DayName').removeClass('show_panel');
+    }
+    
 }
